e6783fd7d1697069f4617f34ba07645bfea846b7
Jainkarn P <jainripper@gmail.com>
2019-05-07 10:17:14 +1000

    Updates

---
+++ precache-manifest.ed484e6fc32fa6054339fb7d42dd2b13.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02ce0708aee3eeecdaf9c8a68c6dcf05",
+    "url": "/confusion-beta/index.html"
+  },
+  {
+    "revision": "13b304f9201f004d9d52",
+    "url": "/confusion-beta/static/css/2.68c766d0.chunk.css"
+  },
+  {
+    "revision": "849ba5e866ebe5d7a5d9",
+    "url": "/confusion-beta/static/css/main.de4043e3.chunk.css"
+  },
+  {
+    "revision": "13b304f9201f004d9d52",
+    "url": "/confusion-beta/static/js/2.b85b03d4.chunk.js"
+  },
+  {
+    "revision": "849ba5e866ebe5d7a5d9",
+    "url": "/confusion-beta/static/js/main.d804455a.chunk.js"
+  },
+  {
+    "revision": "b0c1b4a1a58ae82a5d01",
+    "url": "/confusion-beta/static/js/runtime~main.7e258b1f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/confusion-beta/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/confusion-beta/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/confusion-beta/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/confusion-beta/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/confusion-beta/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
